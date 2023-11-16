32a4ace18bb708785bf823b62f3e69015545fe54
Thaliaraujo <thalia.araujo321@gmail.com>
2023-11-16 14:13:34 -0300

    refactor

--- src/app/pages/dashboard/components/form/components/cardForm/CardForm.tsx
+++ src/app/pages/dashboard/components/form/components/cardForm/CardForm.tsx
@@ -1,12 +1,19 @@
+import React from 'react';
+import {useState} from 'react';
 import styled from 'styled-components';
-
 interface ICardProps{
     label: string;
     icon?: any;
 };
 
+// Utilizando shouldForwardProp
+const ForwardingSCard = React.forwardRef<HTMLDivElement, { isActive: boolean }>(({ isActive, ...props }, ref) => (
+    <SCard ref={ref} isActive={isActive} {...props} />
+));
 
-const SCard = styled.div`
+export { ForwardingSCard as SCard };
+
+const SCard = styled.div<{ isActive: boolean }>`
     display: flex;
     justify-content: space-evenly;
     align-items: center;
@@ -17,7 +24,7 @@ const SCard = styled.div`
     border-radius: 1rem;
     background: var(--neutral-100);
     box-shadow: 0px 4px 10px 0px rgba(31, 37, 89, 0.07), 0px 2px 11px 0px rgba(69, 65, 164, 0.06);
-
+    border: 2px solid ${({ isActive }) => (isActive ? 'var(--primary-color)' : 'transparent')};
     >h3 {
         color: var(--neutral-800);
         font-size: .8rem;
@@ -25,16 +32,19 @@ const SCard = styled.div`
         line-height: 1.25rem; 
         text-align: center;
     }
-
-    &:active {
-        border: 2px solid var(--primary-color);
-    }
 `
 
 export const CardForm: React.FC<ICardProps & { onClick: () => void }> = (props) => {
 
+    const [isActive, setIsActive] = useState(false);
+
+    const handleClick = () => {
+        setIsActive(!isActive);
+        props.onClick();
+    };
+
     return(
-        <SCard onClick={props.onClick}>
+        <SCard onClick={handleClick}  isActive={isActive}>
             <img src={props.icon} alt= {props.label}/>
             <h3>{props.label}</h3>
         </SCard>
